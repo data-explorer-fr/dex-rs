a362ec49be343ce468e49ed24440dfbd86e6ca7d
thomas <thomas@localhost.localdomain>
2021-01-02 22:54:53 +0100

    init: add stub for insee source

---
+++ lib/insee/mod.rs
@@ -0,0 +1,10 @@
+pub mod insee;
+
+fn download_file(path: &str, data_folder: &str, incremental bool) -> Result<()> {
+    Ok();
+}
+
+pub fn get_timestamps() -> Result<()> {
+    Ok();
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
+
+use clap::clap_app;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,9 +11,6 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
